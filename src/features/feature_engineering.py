b79b3dcbf37cbb4ffa5a107df8378cd52494bb48
Soham Mukherjee <129587134+sohamfcb@users.noreply.github.com>
2025-07-05 15:12:12 +0530

    replace bow with tfidf

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 import os
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import yaml
 
@@ -26,7 +26,7 @@ X_test = test_data['content'].values
 y_test = test_data['sentiment'].values
 
 # Apply Bag of Words (CountVectorizer)
-vectorizer = CountVectorizer(max_features=max_features)
+vectorizer = TfidfVectorizer(max_features=max_features)
 
 # Fit the vectorizer on the training data and transform it
 X_train_bow = vectorizer.fit_transform(X_train)
@@ -47,5 +47,5 @@ data_path = os.path.join("data","features")
 
 os.makedirs(data_path)
 
-train_df.to_csv(os.path.join(data_path,"train_bow.csv"))
-test_df.to_csv(os.path.join(data_path,"test_bow.csv"))
+train_df.to_csv(os.path.join(data_path,"train_tfidf.csv"))
+test_df.to_csv(os.path.join(data_path,"test_tfidf.csv"))
